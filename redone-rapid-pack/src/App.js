e111c76caa4beb06b5777628674834325eb27dbd
Matīss Komļevs <matisskomlevs@gmail.com>
2022-06-19 16:49:14 +0300

    Create basic setup for functionality
    Created the necessary component - Homepage
    Added the CSS

--- redone-rapid-pack/src/App.js
+++ redone-rapid-pack/src/App.js
@@ -1,9 +1,21 @@
-
+import React from 'react'
 import './App.css';
+import Homepage from './components/Homepage'
 
 function App() {
   return (
-    <h2>My react app</h2>
+    <div className="App">
+      <div className="navbar p-page"> 
+        <h2>RapidPack</h2>
+      </div>
+
+      <Homepage />
+
+      <footer className="p-page main-footer">
+        <a href="https://www.linkedin.com/in/matiss-komlevs-20343020b/" target="_blank"><p>Linkedin</p></a>
+        <a href="https://github.com/Matt-478" target="_blank"><p>Github</p></a>
+      </footer>
+    </div>
   )
 }
 
---
+++ redone-rapid-pack/src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <h3>
+      HOMEPAGE
+    </h3>
+  )
+}
+
+export default Homepage
